54131145e935e98f2213b44a91fd20b0c9d01ba1
codeatcg <30388546+codeatcg@users.noreply.github.com>
2022-05-06 09:16:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,9 +18,9 @@ tar -zxf ncbi-blast-2.11.0+-x64-linux.tar.gz
 wget -c ftp://ftp.ncbi.nlm.nih.gov/pub/agarwala/windowmasker/windowmasker
 chmod +x windowmasker
 
-ln -s $prefix/third_party/primer3/src/primer3_core $prefix/bin/primer3_core
-ln -s $prefix/third_party/primer3/src/ntthal $prefix/bin/ntthal
-ln -s $prefix/third_party/primer3/settings_files $prefix/settings_files
+ln -s $prefix/third_party/primer3-2.6.1/src/primer3_core $prefix/bin/primer3_core
+ln -s $prefix/third_party/primer3-2.6.1/src/ntthal $prefix/bin/ntthal
+ln -s $prefix/third_party/primer3-2.6.1/settings_files $prefix/settings_files
 
 ln -s $prefix/third_party/ncbi-blast-2.11.1+/bin/makeblastdb $prefix/bin/makeblastdb
 ln -s $prefix/third_party/ncbi-blast-2.11.1+/bin/blastn $prefix/bin/blastn
