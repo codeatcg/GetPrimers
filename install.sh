a442ab0ff1661de61319d7f00ca69b7492b56572
codeatcg <30388546+codeatcg@users.noreply.github.com>
2022-05-09 14:43:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,20 @@ tar -zxf ncbi-blast-2.11.0+-x64-linux.tar.gz
 #wget -c ftp://ftp.ncbi.nlm.nih.gov/pub/agarwala/windowmasker/windowmasker
 #chmod +x windowmasker
 
+cp $prefix/third_party/primer3-2.6.1/settings_files/primer3web_v0_4_0_default_settings.txt $prefix/third_party/primer3-2.6.1/settings_files/primer3web_v0_4_0_modify_settings.txt
+sed -i -e 's/PRIMER_MAX_END_GC=5/PRIMER_MAX_END_GC=4/' \
+-e 's/PRIMER_TM_FORMULA=0/PRIMER_TM_FORMULA=1/' \
+-e 's/PRIMER_SALT_DIVALENT=0.0/PRIMER_SALT_DIVALENT=1.5/' \
+-e 's/PRIMER_DNTP_CONC=0.0/PRIMER_DNTP_CONC=0.6/' \
+-e 's/PRIMER_SALT_CORRECTIONS=0/PRIMER_SALT_CORRECTIONS=1/' \
+-e 's/PRIMER_MAX_POLY_X=5/PRIMER_MAX_POLY_X=4/' \
+-e 's/PRIMER_MIN_THREE_PRIME_DISTANCE=-1/PRIMER_MIN_THREE_PRIME_DISTANCE=1/' $prefix/third_party/primer3-2.6.1/settings_files/primer3web_v0_4_0_modify_settings.txt
+
+cp $prefix/third_party/primer3-2.6.1/settings_files/primer3web_v4_0_0_default_settings.txt $prefix/third_party/primer3-2.6.1/settings_files/primer3web_v4_0_0_modify_settings.txt
+sed -i -e 's/PRIMER_MIN_THREE_PRIME_DISTANCE=3/PRIMER_MIN_THREE_PRIME_DISTANCE=1/' \
+-e 's/PRIMER_PAIR_MAX_DIFF_TM=5.0/#PRIMER_PAIR_MAX_DIFF_TM=5.0/' \
+-e 's/PRIMER_MAX_END_GC=5/PRIMER_MAX_END_GC=4/' $prefix/third_party/primer3-2.6.1/settings_files/primer3web_v4_0_0_modify_settings.txt
+
 ln -s $prefix/third_party/primer3-2.6.1/src/primer3_core $prefix/bin/primer3_core
 ln -s $prefix/third_party/primer3-2.6.1/src/ntthal $prefix/bin/ntthal
 ln -s $prefix/third_party/primer3-2.6.1/settings_files $prefix/settings_files
